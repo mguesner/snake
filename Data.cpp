e7ee8521867502dba78c9b8adb31e1dda3946ceb
Nicolas GUEZELLOT PRUDHOMME <nguezell@e1r10p4.42.fr>
2015-03-06 14:40:17 +0100

    TIENS COUILLON

---
+++ Data.cpp
@@ -0,0 +1,26 @@
+#include "Data.hpp"
+
+Data::Data()
+{
+	score = 1;
+}
+
+void Data::SetScore(int value)
+{
+	score = value;
+}
+
+int Data::GetScore()
+{
+	return score;
+}
+
+void Data::SetChoice(int choice)
+{
+	this->choice = choice;
+}
+
+void Data::SetState(eGameState state)
+{
+	this->state = state;
+}
